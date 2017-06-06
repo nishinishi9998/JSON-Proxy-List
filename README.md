e8d667e80d32c6585f96f9844703c6d3f206a389
Alvaro Fernandez <nishinishi9999@gmail.com>
2017-06-06 20:17:46 +0200

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# JSON-Proxy-List
-JSON Proxy List
--- main.js
+++
@@ -1,7 +0,0 @@
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var addr = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
-
-server.listen(port, addr, function()
-    {
-        console.log('Listening on'+addr+':'+port.toString());
-    });
